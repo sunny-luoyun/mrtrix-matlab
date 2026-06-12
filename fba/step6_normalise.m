5c6b26ca3fcabaedeb25ef51f3c986c3174121f9
sunny-luoyun <101499384+sunny-luoyun@users.noreply.github.com>
2026-06-12 10:14:45 +0800

    feat: mtnormalise 根据壳层数自动选择 2 组织或 3 组织归一化

--- fba/step6_normalise.m
+++ fba/step6_normalise.m
@@ -1,5 +1,18 @@
 function step6_normalise(workPath, subList, csdAlgo)
     fbaSubDir = fullfile(workPath, 'fba', 'subjects');
+    isMultiShell = false;
+    if strcmp(csdAlgo, 'msmt_csd') && ~isempty(subList)
+        sub = subList{1};
+        dwiFile = fullfile(fbaSubDir, sub, 'dwi_upsampled.mif');
+        if ~exist(dwiFile, 'file')
+            dwiFile = fullfile(fbaSubDir, sub, 'dwi.mif');
+        end
+        if exist(dwiFile, 'file')
+            [~, cmdout] = system(sprintf('mrinfo -shell_bvalues %s', dwiFile));
+            bvals = sscanf(cmdout, '%f');
+            isMultiShell = sum(bvals > 0) >= 2;
+        end
+    end
     for i = 1:length(subList)
         sub = subList{i};
         subDir = fullfile(fbaSubDir, sub);
@@ -19,8 +32,13 @@ function step6_normalise(workPath, subList, csdAlgo)
             system(sprintf('maskfilter %s erode %s -npass 1 -force', origMaskFile, maskFile));
         end
         if strcmp(csdAlgo, 'msmt_csd')
-            cmd = sprintf('mtnormalise %s/wmfod.mif %s/wmfod_norm.mif %s/csf.mif %s/csf_norm.mif -mask %s -force', ...
-                subDir, subDir, subDir, subDir, maskFile);
+            if isMultiShell
+                cmd = sprintf('mtnormalise %s/wmfod.mif %s/wmfod_norm.mif %s/gm.mif %s/gm_norm.mif %s/csf.mif %s/csf_norm.mif -mask %s -force', ...
+                    subDir, subDir, subDir, subDir, subDir, subDir, maskFile);
+            else
+                cmd = sprintf('mtnormalise %s/wmfod.mif %s/wmfod_norm.mif %s/csf.mif %s/csf_norm.mif -mask %s -force', ...
+                    subDir, subDir, subDir, subDir, maskFile);
+            end
         else
             cmd = sprintf('mtnormalise %s/wmfod.mif %s/wmfod_norm.mif -mask %s -force', ...
                 subDir, subDir, maskFile);
