4899531d1d122435791bcca40406d7e2de1c4e43
sunny-luoyun <101499384+sunny-luoyun@users.noreply.github.com>
2026-06-12 09:26:07 +0800

    fix: mtnormalise 使用腐蚀后的保守 mask，避免非脑体素导致平衡因子异常

--- fba/step6_normalise.m
+++ fba/step6_normalise.m
@@ -3,16 +3,20 @@ function step6_normalise(workPath, subList, csdAlgo)
     for i = 1:length(subList)
         sub = subList{i};
         subDir = fullfile(fbaSubDir, sub);
-        maskFile = fullfile(subDir, 'dwi_mask_upsampled.mif');
+        origMaskFile = fullfile(subDir, 'dwi_mask_upsampled.mif');
         if ~exist(fullfile(subDir, 'wmfod.mif'), 'file')
             continue;
         end
-        if ~exist(maskFile, 'file')
+        if ~exist(origMaskFile, 'file')
             dwiFile = fullfile(subDir, 'dwi_upsampled.mif');
             if ~exist(dwiFile, 'file')
                 dwiFile = fullfile(subDir, 'dwi.mif');
             end
-            system(sprintf('dwi2mask %s %s -force', dwiFile, maskFile));
+            system(sprintf('dwi2mask %s %s -force', dwiFile, origMaskFile));
+        end
+        maskFile = fullfile(subDir, 'dwi_mask_conservative.mif');
+        if ~exist(maskFile, 'file')
+            system(sprintf('maskfilter %s erode %s -npass 1 -force', origMaskFile, maskFile));
         end
         if strcmp(csdAlgo, 'msmt_csd')
             cmd = sprintf('mtnormalise %s/wmfod.mif %s/wmfod_norm.mif %s/gm.mif %s/gm_norm.mif %s/csf.mif %s/csf_norm.mif -mask %s -force', ...
