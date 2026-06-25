7e3c9203ac9db210438ef821ae7d8035f69a4f1d
sunny-luoyun <101499384+sunny-luoyun@users.noreply.github.com>
2026-06-25 22:38:29 +0800

    清理：删除未使用的 5tt_coreg / gmwmSeed_coreg 生成步骤

--- pre/T1corg.m
+++ pre/T1corg.m
@@ -3,8 +3,6 @@ function T1corg(path, name, work, startname)
     output = fullfile(work, 'T1_corg', name); 
     mkdir(output); % 创建目录
 
-    Tcoreg = fullfile(work, 'T1_coreg', name);
-
     T = fullfile(work,'T1mif',name);
 
     b0_path = fullfile(work, 'pred_b0', name);
@@ -15,15 +13,6 @@ function T1corg(path, name, work, startname)
     system(Tc);
 
     Tc = sprintf('5tt2gmwmi %s/5tt.mif %s/gmwmSeed.mif -force', ... 
-        output, output);
-    system(Tc);
-
-    % 配准的分割
-    Tc = sprintf('5ttgen fsl %s/T1_coreg.nii.gz %s/5tt_coreg.mif -force', ... 
-        Tcoreg, output);
-    system(Tc);
-
-    Tc = sprintf('5tt2gmwmi %s/5tt_coreg.mif %s/gmwmSeed_coreg.mif -force', ... 
         output, output);
     system(Tc);
 
